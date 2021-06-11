fd52b65d9489797a20019e36e56166e2684f96d8
James Fleming <james@electronic-quill.net>
2021-06-11 20:35:22 +0200

    Flesh out the schema-handling

---
+++ src/core-schema.ts
@@ -0,0 +1,92 @@
+import {
+  IncomingSchemaVersion,
+  IncomingResourceType,
+  SchemaResourceTypeAttribute,
+  IncomingSchemaRelationship,
+  IncomingSchemaRelationshipAttribute,
+} from './schema';
+
+const coreSchema = new IncomingSchemaVersion({
+  version: 1,
+  applyOrder: 0,
+  resourceTypes: [
+    new IncomingResourceType({
+      name: "tags",
+      dependent: false,
+      notes: "Special-case meta-resource, representing an instance of any type of resource.",
+      attributes: [
+        new SchemaResourceTypeAttribute({
+          name: "description",
+          description: "Clarification of what the tag means.",
+          values: undefined
+        })
+      ]}),
+    new IncomingResourceType({
+      name: "any",
+      dependent: false,
+      notes: "Special-case meta-resource, representing an instance of any type of resource.",
+      attributes: [],
+    }),
+    new IncomingResourceType({
+      name: "pronouns",
+      dependent: false,
+      notes: "The pronouns by which a person prefers to be addressed",
+      attributes: [],
+    }),
+    new IncomingResourceType({
+      name: "people",
+      dependent: false,
+      notes: "Any kind of person, whether real, notional or purely speculative.",
+      attributes: [
+        new SchemaResourceTypeAttribute({
+          name: "displayName",
+          description: "The human-friendly version of their name, to be displayed in the UI.",
+          values: undefined
+        })
+      ]
+    }),
+    new IncomingResourceType({
+      name: "groups",
+      dependent: false,
+      notes: "A means of grouping things into sets.",
+      attributes: [
+        new SchemaResourceTypeAttribute({
+          name: "description",
+          description: "Clarifying notes about what this group is for.",
+          values: undefined
+        })
+      ]
+    }),
+  ],
+  relationships: [
+    new IncomingSchemaRelationship({
+      name: "Tags",
+      sourceType: "any",
+      targetType: "tags",
+      cardinality: "many:many",
+      dependent: false,
+      notes: "Anything can be tagged with any tag.",
+      attributes: []
+    }),
+    new IncomingSchemaRelationship({
+      name: "Groups",
+      sourceType: "any",
+      targetType: "any",
+      cardinality: "many:many",
+      dependent: false,
+      notes: "Anything can be put into a group.",
+      attributes: []
+    }),
+    new IncomingSchemaRelationship({
+      name: "Pronouns",
+      sourceType: "people",
+      targetType: "pronouns",
+      cardinality: "many:many",
+      dependent: false,
+      notes: "Sets of pronouns a person is _comfortable_ being addressed with; could be more than one.",
+      attributes: []
+    })
+  ]
+})
+
+export { coreSchema };
--- src/schema.ts
+++ src/schema.ts
@@ -1,26 +1,135 @@
-class SchemaResourceType {
-  name: string;
-  dependent: boolean;
-  notes: string;
-  attributes: [SchemaResourceTypeAttribute];
-  relationships: [string];
+// Incoming classes, for inserting new schemas
 
-  constructor(name, dependent, notes, attributes, relationships) {
+class IncomingSchemaVersion {
+  readonly version: number;
+  readonly applyOrder: number;
+  // resourceTypes is a map of string:object, where
+  // string = name of a SchemaResourceType object, and
+  // object = SchemaResourceType object.
+  readonly resourceTypes: [IncomingResourceType];
+  readonly relationships: [IncomingSchemaRelationship];
+
+  constructor({version, applyOrder, resourceTypes, relationships}) {
+    this.version = version;
+    this.applyOrder = applyOrder;
+    this.resourceTypes = resourceTypes;
+    this.relationships = relationships;
+  }
+}
+
+
+class IncomingResourceType {
+  readonly name: string;
+  readonly dependent: boolean;
+  readonly notes: string;
+  attributes: [SchemaResourceTypeAttribute];
+
+  constructor({name, dependent, notes, attributes}) {
     this.name = name;
     this.dependent = dependent;
     this.notes = notes;
     this.attributes = attributes;
-    this.relationships = relationships
+  }
+}
+
+
+class IncomingSchemaRelationship {
+  readonly name: string;
+  readonly sourceType: string;
+  readonly targetType: string;
+  readonly cardinality: SchemaCardinality;
+  readonly dependent: boolean;
+  readonly notes: string;
+  attributes: [IncomingSchemaRelationshipAttribute];
+
+  constructor({
+    name,
+    sourceType,
+    targetType,
+    cardinality,
+    dependent,
+    notes,
+    attributes
+  }) {
+    this.name = name;
+    this.sourceType = sourceType;
+    this.targetType = targetType;
+    this.cardinality = cardinality;
+    this.dependent = dependent;
+    this.notes = notes;
+    this.attributes = attributes;
+  }
+}
+
+class IncomingSchemaRelationshipAttribute {
+  readonly name: string;
+  readonly description: string;
+  // "values" will need to be a union type at some point
+  values: [string];
+
+  constructor({name, description, values}) {
+    this.name = name;
+    this.description = description;
+    this.values = values;
+  }
+}
+
+
+// Classes for extracting the schema from the db and building the API
+
+class SchemaSet {
+  readonly name: string;
+  // The `schemas` attribute is a map of number:object, where
+  // number == applyOrder of the schema version
+  // object == SchemaVersion object
+  // This gathers them under a single, specified struture.
+  schemas: object;
+
+  constructor(name, schemas) {
+    this.name = name;
+    this.schemas = schemas;
+  }
+}
+
+class SchemaVersion {
+  readonly version: number;
+  readonly applyOrder: number;
+  readonly createdAt: number;
+  // resourceTypes is a map of string:object, where
+  // string = name of a SchemaResourceType object, and
+  // object = SchemaResourceType object.
+  resourceTypes: [object];
+
+  constructor({version, applyOrder, resourceTypes}) {
+    this.version = version;
+    this.applyOrder = applyOrder;
+    this.resourceTypes = resourceTypes;
+  }
+}
+
+class SchemaResourceType {
+  readonly name: string;
+  readonly dependent: boolean;
+  readonly notes: string;
+  attributes: [SchemaResourceTypeAttribute];
+  relationships: [string];
+
+  constructor({name, dependent, notes, attributes, relationships}) {
+    this.name = name;
+    this.dependent = dependent;
+    this.notes = notes;
+    this.attributes = attributes;
+    this.relationships = relationships;
   }
 }
 
 class SchemaResourceTypeAttribute {
-  name: string;
-  description: string;
+  readonly name: string;
+  readonly description: string;
   // "values" will need to be a union type at some point
   values: [string];
 
-  constructor(name, description, values) {
+  constructor({name, description, values}) {
     this.name = name;
     this.description = description;
     this.values = values;
@@ -35,21 +144,39 @@ enum SchemaCardinality {
 }
 
 class SchemaRelationship {
-  name: string;
-  targetType: SchemaResourceType;
-  cardinality: SchemaCardinality;
-  dependent: boolean;
-  notes: string;
+  readonly name: string;
+  readonly targetType: SchemaResourceType;
+  readonly cardinality: SchemaCardinality;
+  readonly dependent: boolean;
+  readonly notes: string;
+  attributes: [SchemaRelationshipAttribute];
 
-  constructor(name, targetType, cardinality, dependent, notes) {
+  constructor({name, targetType, cardinality, dependent, notes, attributes}) {
     this.name = name;
     this.targetType = targetType;
     this.cardinality = cardinality;
     this.dependent = dependent;
     this.notes = notes;
+    this.attributes = attributes;
   }
 }
 
+class SchemaRelationshipAttribute {
+  readonly name: string;
+  readonly description: string;
+  // "values" will need to be a union type at some point
+  values: [string];
+
+  constructor({name, description, values}) {
+    this.name = name;
+    this.description = description;
+    this.values = values;
+  }
+}
+
+
+
+// Functions and methods for interacting with the schema
 
 function FetchSchemaFromDb(driver) {
   console.log('Attempting to fetch the schema.');
@@ -69,6 +196,11 @@ export {
   // Types
   SchemaCardinality,
   // Classes
+  IncomingSchemaVersion,
+  IncomingResourceType,
+  IncomingSchemaRelationship,
+  IncomingSchemaRelationshipAttribute,
+  SchemaVersion,
   SchemaResourceType,
   SchemaResourceTypeAttribute,
   SchemaRelationship,
